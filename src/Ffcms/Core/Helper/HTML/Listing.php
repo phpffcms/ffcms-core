f312878fdfc50a4e6f71bbc893790c0a5911c6a3
Mihail <zenn1989@gmail.com>
2015-03-31 21:38:00 +0300

    Build "list" algo done

--- src/Ffcms/Core/Helper/HTML/Listing.php
+++ src/Ffcms/Core/Helper/HTML/Listing.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Ffcms\Core\Helper\HTML;
+use Core\App;
+use Core\Helper\Integer;
 
 /**
  * Class HList
@@ -10,15 +12,71 @@ class Listing {
 
 
     /**
+     * Construct listing elements,property's and future's
      * @param array $elements
      * @return string
      */
     public static function display($elements)
     {
-        if(!in_array($elements['type'], ['ul', 'ol']))
+        if(!in_array($elements['type'], ['ul', 'ol']) || sizeof($elements['items']) < 1)
             return null;
+
         $ulProperties = null;
-        if(sizeof($elements['']))
-        return null;
+        if(sizeof($elements['ul'])) {
+            $ulProperties .= ' class="' . implode(' ', $elements['ul']) . '"';
+        }
+
+        $items = null;
+        foreach($elements['items'] as $item) {
+            if(!in_array($item['type'], ['text', 'link']))
+                continue;
+            if($item['type'] == 'link') {
+                $controllerAction = trim(is_array($item['link']) ? $item['link'][0] : $item['link'], '/');
+                $currentCA = strtolower(App::$Request->getController() . '/' . App::$Request->getAction());
+                if($currentCA == $controllerAction) {
+                    if(!is_null($item['link'][1])) {
+                        if($item['link'][1] == App::$Request->getID())
+                            $item['property'][] = $item['activeClass'];
+                    } else {
+                        $item['property'][] = $item['activeClass'];
+                    }
+                }
+            }
+            $items .= '<li' . (sizeof($item['property']) > 0 ? ' class="' . implode(' ', $item['property']) . '"' : null) . '>';
+            if($item['type'] == 'text') {
+                $items .= ($item['html'] ? $item['text'] : App::$Security->strip_tags($item['text']));
+            } elseif($item['type'] == 'link') {
+                $link = App::$Alias->baseUrl;
+                if(is_array($item['link'])) {
+                    $link .= trim($item['link'][0], '/'); // controller/action
+                    if(!is_null($item['link'][1]))
+                        $link .= '/' . $item['link'][1]; // param id
+                    if(!is_null($item['link'][2]))
+                        $link .= '/' . $item['link'][2]; // param id
+                    if(is_array($item['link'][3])) { // dynamic params ?a=b&v=c etc
+                        $firstParam = true;
+                        foreach($item['link'][3] as $p => $v) {
+                            if($firstParam)
+                                $link .= "?" . $p . '=' . $v;
+                            else
+                                $link .= "&" . $p . '=' . $v;
+                            $firstParam = false;
+                        }
+                    } else {
+                        $link .= '/';
+                    }
+                } else {
+                    $link .= trim($item['link'], '/');
+                }
+                $htmlLink = '<a href="' . App::$Security->escapeQuotes(App::$Security->strip_tags($link)) . '"';
+                if(is_array($item['linkProperty']))
+                    $htmlLink .= ' class="' . implode(' ', $item['linkProperty']) . '"';
+                $htmlLink .= '>' . App::$Security->strip_tags($item['text']) . '</a>';
+                $items .= $htmlLink;
+            }
+            $items .= '</li>';
+        }
+
+        return '<ul' . $ulProperties . '>' . $items . '</ul>';
     }
 }
--- src/Ffcms/Core/Helper/Integer.php
+++ src/Ffcms/Core/Helper/Integer.php
@@ -7,11 +7,32 @@ class Integer {
     const MAX = 2147483647;
 
     /**
+     * Check is current variable is integer
+     * @param int $integer
+     * @return bool
+     */
+    public static function is($integer)
+    {
+        return is_int($integer);
+    }
+
+    /**
+     * Check is current variable seems like integer. Example - string variable only with integer values.
+     * @param mixed $integer
+     * @return bool
+     */
+    public static function isLike($integer)
+    {
+        return filter_var($integer, FILTER_VALIDATE_INT) !== false;
+    }
+
+
+    /**
      * Random Integer with $sequence. Ex: randomInt(2) = 1..9 * 10 ^ 2
      * @param Integer $sequence - sequence of random calculation
      * @return number
      */
-    public function random($sequence)
+    public static function random($sequence)
     {
         $start = pow(10, $sequence - 1);
         $end = pow(10, $sequence);
