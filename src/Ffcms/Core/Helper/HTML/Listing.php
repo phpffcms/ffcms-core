06b1fdcd870ca4dceed4509782565caff6914d51
Mihail Pyatinskyi <zenn1989@gmail.com>
2015-03-31 16:33:08 +0300

    HTML element helpers

---
+++ src/Ffcms/Core/Helper/HTML/Listing.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ffcms\Core\Helper\HTML;
+
+/**
+ * Class HList
+ * @package Ffcms\Core\Helper\HTML
+ */
+class Listing {
+
+
+    /**
+     * @param array $elements
+     * @return string
+     */
+    public static function display($elements)
+    {
+        if(!in_array($elements['type'], ['ul', 'ol']))
+            return null;
+        $ulProperties = null;
+        if(sizeof($elements['']))
+        return null;
+    }
+}
