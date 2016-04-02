6967ab4b06b722ec3163684786bc7757c467267c
zenn <zenn1989@gmail.com>
2016-04-02 12:11:41 +0300

    Rework exceptions. Small typos

--- src/Ffcms/Core/Exception/NativeException.php
+++ src/Ffcms/Core/Exception/NativeException.php
@@ -4,35 +4,67 @@ namespace Ffcms\Core\Exception;
 
 use Ffcms\Core\Helper\Type\Str;
 
+/**
+ * Class NativeException. Work around throwed exception to display it
+ */
 class NativeException extends \Exception
 {
     protected $message;
 
+    /**
+     * NativeException constructor. Pass message insde the exception
+     * @param string|null $message
+     */
     public function __construct($message = null) {
         if ($message !== null) {
             $this->message = $message;
         }
     }
 
+    /**
+     * Display native exception
+     * @param string $message
+     * @return string|unknown
+     */
     public function display($message = null)
     {
+        // if passed message is null get exception msg
         if ($message === null) {
             $message = $this->message;
         }
 
+        // hide root path from exception
         $message = Str::replace(root, '$DOCUMENT_ROOT', $message);
         $message = htmlentities($message);
-
-        if (type === 'web') {
-            header('HTTP/1.1 404 Not Found');
-            return $this->rawHTML($message);
+        
+        // generate response based on environment type
+        switch (env_type) {
+            case 'html':
+                return $this->rawHTML($message);
+            case 'json':
+                return $this->sendJSON($message);
         }
-
+        
         return $message;
     }
 
-    protected function rawHTML($message = null)
+    /**
+     * Build html response
+     * @param unknown $message
+     */
+    protected function sendHTML($message = null)
     {
+        header('HTTP/1.1 404 Not Found');
         return '<!DOCTYPE html><html><head><title>An error has occurred</title></head><body><div style="width:60%; margin: auto; background-color: #fcc;border: 1px solid #faa; padding: 0.5em 1em;"><h1 style="font-size: 120%">Runtime error</h1><p>' . $message . '</p></div></body></html>';
+    }
+    
+    /**
+     * Build json response
+     * @param string $message
+     */
+    protected function sendJSON($message = NULL) 
+    {
+        header('Content-Type: application/json');
+        return json_encode(['status' => 0, 'message' => $message]);
     }
 }
--- src/Ffcms/Core/Exception/TemplateException.php
+++ src/Ffcms/Core/Exception/TemplateException.php
@@ -5,7 +5,6 @@ namespace Ffcms\Core\Exception;
 
 use Ffcms\Core\App;
 use Ffcms\Core\Arch\Controller;
-use Ffcms\Core\Helper\Type\Obj;
 use Ffcms\Core\Helper\Type\Str;
 
 abstract class TemplateException extends \Exception
@@ -15,35 +14,66 @@ abstract class TemplateException extends \Exception
     public $text = 'An unexpected error occurred: %e%';
     public $tpl = 'default';
 
+    /**
+     * Display exception template
+     * @throws JsonException
+     * @throws \Exception
+     * @return string
+     */
     public function display()
     {
+        // hide path root and stip tags from exception message
         $msg = Str::replace(root, '$DOCUMENT_ROOT', $this->getMessage());
         $msg = App::$Security->strip_tags($msg);
+        // get message translation
         $this->text = App::$Translate->get('Default', $this->text, ['e' => $msg]);
-
+        
+        // if exception is throwed not from html-based environment
+        if (env_type !== 'html') {
+            if (env_type === 'json') {
+                return (new JsonException($msg))->display();
+            } else {
+                return $this->text;
+            }
+        }
+        
+        // add notification in debug bar if exist
         if (App::$Debug !== null) {
             App::$Debug->addException($this);
         }
-
+        // return rendered result
+        return $this->buildFakePage();
+    }
+    
+    /**
+     * Build fake page with error based on fake controller initiation
+     */
+    protected function buildFakePage()
+    {
+        // initialize fake controller to display page with exception
         $fakeController = new Controller();
+        // check if used no layout template
         if (defined('env_no_layout') && env_no_layout === true) {
             $fakeController->layout = null;
         }
-
+        
+        // add global title tag value
         $fakeController->setGlobalVar('title', App::$Translate->get('Default', $this->title));
+        // build error text
         $rawResponse = 'error';
         try {
-            $rawResponse = App::$View->render('errors/' . $this->tpl, ['msg' => $this->text]);
+            $rawResponse = App::$View->render('native/errors/' . $this->tpl, ['msg' => $this->text]);
             if (Str::likeEmpty($rawResponse)) {
                 $rawResponse = $this->text;
             }
         } catch (SyntaxException $e) {
             $rawResponse = $this->text;
         }
+        // set controller body response
         $fakeController->setResponse($rawResponse);
-
+        // set status code for header
         App::$Response->setStatusCode((int)$this->status);
-
+        // return compiled html output
         return $fakeController->getOutput();
     }
 
