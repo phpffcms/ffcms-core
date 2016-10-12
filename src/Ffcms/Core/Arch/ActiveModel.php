55fd9e68a75ec1fd1f259676a86b5b055c37b61b
zenn <zenn1989@gmail.com>
2016-10-12 22:44:44 +0300

    Extend laravel eloquent casts mutators by serialize
    Now serialized by locale columns can be defined as native array.
    Required in ActiveRecord: protected $casts = ['column' => 'serialize'];

--- src/Ffcms/Core/Arch/ActiveModel.php
+++ src/Ffcms/Core/Arch/ActiveModel.php
@@ -11,6 +11,7 @@ use \Illuminate\Database\Eloquent\Model as LaravelModel;
  * Class ActiveModel. Basic implementation of laravel active records model with predefined settings
  * @package Ffcms\Core\Arch
  * @method static ActiveModel where($field = null, $compare = null, $value = null)
+ * @method static ActiveModel whereIn($field = null, array $values = [])
  * @method static ActiveModel orWhere($field = null, $compare = null, $value = null)
  * @method static ActiveModel orderBy($field, $sortType)
  * @method static ActiveModel|null first()
@@ -36,5 +37,68 @@ class ActiveModel extends LaravelModel
         }
 
         return Serialize::getDecodeLocale($this->{$attribute});
+    }
+
+    /**
+     * Set model attribute. Extend laravel attribute casting mutators by serialized array
+     * @param string $key
+     * @param mixed $value
+     * @return LaravelModel
+     */
+    public function setAttribute($key, $value)
+    {
+        if ($value !== null && $this->isSerializeCastable($key)) {
+            $value = $this->asSerialize($value);
+        }
+        return parent::setAttribute($key, $value);
+    }
+
+    /**
+     * Cast model attribute. Extend laravel attribute casting mutators by serialized array
+     * @param string $key
+     * @param mixed $value
+     * @return mixed
+     */
+    protected function castAttribute($key, $value)
+    {
+        if ($value === null) {
+            return $value;
+        }
+
+        if ($this->getCastType($key) === 'serialize') {
+            return $this->fromSerialize($value);
+        }
+
+        return parent::castAttribute($key, $value);
+    }
+
+    /**
+     * Check if key is castable to be serialized
+     * @param string $key
+     * @return bool
+     */
+    public function isSerializeCastable($key)
+    {
+        return $this->hasCast($key, 'serialize');
+    }
+
+    /**
+     * Serialize value
+     * @param $value
+     * @return Serialize
+     */
+    public function asSerialize($value)
+    {
+        return serialize($value);
+    }
+
+    /**
+     * Unserialize value
+     * @param $value
+     * @return mixed
+     */
+    public function fromSerialize($value)
+    {
+        return unserialize($value);
     }
 }
