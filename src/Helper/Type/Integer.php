0616fba3feb9d82cb084da8e7b6eb7a9412bd50c
zenn <zenn1989@ya.ru>
2018-11-23 15:32:14 +0300

    typo fix

--- src/Helper/Type/Integer.php
+++ src/Helper/Type/Integer.php
@@ -12,10 +12,10 @@ class Integer
 
     /**
      * Random Integer with $sequence. Ex: randomInt(2) = 1..9 * 10 ^ 2
-     * @param Integer $sequence - sequence of random calculation
-     * @return number
+     * @param int $sequence - sequence of random calculation
+     * @return int
      */
-    public static function random($sequence)
+    public static function random(int $sequence): ?int
     {
         $start = pow(10, $sequence - 1);
         $end = pow(10, $sequence);
