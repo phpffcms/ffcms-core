d19741bd96b0bbb2f2801568a582701373fce9ae
zenn <zenn1989@ya.ru>
2019-01-10 15:43:32 +0300

    rework unit tests

--- tests/_support/App.php
+++ tests/_support/App.php
@@ -2,20 +2,20 @@
 
 namespace Ffcms\Core;
 
-use Ffcms\Core\Arch\View;
+
 use Ffcms\Core\Cache\MemoryObject;
 use Ffcms\Core\Helper\Security;
 use Ffcms\Core\I18n\Translate;
-use Ffcms\Core\Managers\CronManager;
-use Ffcms\Core\Managers\EventManager;
 use Ffcms\Core\Network\Request;
 use Ffcms\Core\Network\Response;
+
+use Codeception\Module;
 
 /**
  * Class App. This class provide fake main entry-point object (App::$Obj) with faked static variables.
  * @package Ffcms\Core
  */
-class App extends \Codeception\Module
+class App extends Module
 {
     public static $Memory;
     public static $Properties;
@@ -36,10 +36,12 @@ class App extends \Codeception\Module
     private $loader;
 
     /**
-     * App constructor. Initialize fake mock object
+     * Initialize app construction.
+     * @throws Exception\NativeException
      */
-    public function __construct()
+    public static function init()
     {
+        // define global vars
         $root = realpath(__DIR__ . '/../../../../../');
         // define root ;)
         if (!defined('root')) {
@@ -51,13 +53,7 @@ class App extends \Codeception\Module
         }
         // include bootstrap autoloader
         include $root . '/Loader/Autoload.php';
-    }
 
-    /**
-     * Initialize app construction.
-     */
-    public function init()
-    {
         // initialize memory and properties controllers, will work fine in test environment
         self::$Memory = MemoryObject::instance();
         self::$Properties = new Properties();
@@ -75,11 +71,12 @@ class App extends \Codeception\Module
     /**
      * Make fake factory method for obj building. Just singleton logic, fake factory
      * @return App
+     * @throws Exception\NativeException
      */
     public static function factory()
     {
-        if (self::$instance === null) {
-            self::$instance = new self();
+        if (!self::$instance) {
+            self::$instance = self::init();
         }
         return self::$instance;
     }
---
+++ tests/unit/Helper/Type/AnyTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zenn1
+ * Date: 10.01.2019
+ * Time: 15:20
+ */
+
+namespace Ffcms\Core\Helper\Type;
+
+
+class AnyTest
+{
+
+}
