c838c1974a8ec1b7e643d8955538a11faaa52000
Piatinskii M <zenn1989@gmail.com>
2022-08-10 09:03:10 +0300

    add status code parameter

--- src/Network/Response.php
+++ src/Network/Response.php
@@ -16,15 +16,16 @@ class Response extends FoundationResponse
     /**
      * Fast redirect in web environment
      * @param string $to
+     * @param int $code
      * @param bool $full
      */
-    public function redirect($to, $full = false)
+    public function redirect($to, $full = false, $code = 302)
     {
         $to = trim($to, '/');
         if (!$full && !Str::startsWith(App::$Alias->baseUrl, $to)) {
             $to = App::$Alias->baseUrl . '/' . $to;
         }
-        $redirect = new FoundationRedirect($to);
+        $redirect = new FoundationRedirect($to, $code);
         $redirect->send();
         exit('Redirecting to ' . $to . ' ...');
     }
